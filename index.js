bd5aeedf20013818f41162eb1a2b3ac23c80815c
Jared W Kelnhofer <814203391@wwcc.edu>
2019-05-21 11:48:57 -0700

    Added variable port functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8000;   //This is for Heroku. We will use the port that is available, OR the port 8000.
 
 app.use(express.static('./dist'));
 
-app.listen(8000, () => console.log('ready'));
+app.listen(port, () => console.log('ready'));
