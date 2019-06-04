3c5ebce421c209bb0326110e99c87a90379a9dc9
Jared W Kelnhofer <814203391@wwcc.edu>
2019-06-04 11:46:38 -0700

    v2

---
+++ app.js
@@ -0,0 +1,59 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const ColorService = require('./services/color-service');
+
+$(document).ready(() => {
+    $('#searchBtn').on('click', () => {
+        let searchText = $('#searchText').val();
+
+        ColorService
+            .searchColors(searchText)
+            .then((results) => {
+                const paletteTemplate = $('#paletteTemplate');
+                const paletteHTML = paletteTemplate.html().trim();
+                const output = $('#output');
+
+                results.forEach((palette)=> {
+                    let $palette = $(paletteHTML);
+
+                    let $paletteImage = $palette.find('.palette-image');
+                    let $paletteName = $palette.find('.palette-name');
+                    let $paletteAuthor = $palette.find('.palette-author');
+
+                    $paletteImage.attr('src', palette.imageUrl);
+                    $paletteName.text(palette.title);
+                    $paletteAuthor.text(palette.userName);
+
+                    output.append($palette);
+                });
+            })
+            .catch((err) => {
+                alert('shit hit the fan');
+                console.error('failed: ', err);
+            });
+    });
+});
+},{"./services/color-service":2}],2:[function(require,module,exports){
+module.exports = {
+    searchColors: searchColors
+};
+
+const colourLoversAPI = '//www.colourlovers.com/api/colors';
+
+function searchColors(query, filters = {}) {
+    let queryParams = [];
+    for( let key in filters ) {
+        queryParams.push(`${key}=${filters[key]}`);
+    }
+    queryParams.push(`keywords=${query}`);
+    queryParams.push('jsonCallback=?');
+
+    let searchUrl = `${colourLoversAPI}?${queryParams.join('&')}`;
+
+    return new Promise((resolve, reject) => {
+        $.getJSON(searchUrl, resolve)
+            .fail((jqxhr, textStatus, error) => {
+                reject(error);
+            });
+    });
+}
+},{}]},{},[1]);
